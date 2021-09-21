d3695c0496cc32cdab5926e67c7b04559c4c2508
ABU-SHAMA <ABU-SHAMA@unrwa.org>
2021-09-21 15:15:46 +0300

    Changed order of lines

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const MongoStore = require('connect-mongo')
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
-const app = express();
+
 
 //'mongodb://localhost:27017/yelp-camp'
 mongoose.connect(dbUrl, {
@@ -37,6 +37,8 @@ db.on('error', console.error.bind(console, 'connection error'))
 db.once('open', () => {
     console.log('DATABASE CONNECTED')
 })
+
+const app = express();
 
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
