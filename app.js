117563c5fb5adcdb156a0c43f92c0d70cf9d942a
ABU-SHAMA <ABU-SHAMA@unrwa.org>
2021-09-21 19:23:40 +0300

    Changed order of line

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
-app.use((req, res, next) => {
+app.use(function (req, res, next) {
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success')
     res.locals.error = req.flash('error')
